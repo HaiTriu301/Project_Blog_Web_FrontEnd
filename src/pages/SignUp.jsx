4d6db7c9e3c95a285cd27862f4a4fea3bcfe69de
HaiTriu301 <haitriuwork30@gmail.com>
2025-12-04 15:11:28 +0700

    Update sign up

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,13 +1,13 @@
 import {Link} from "react-router-dom";
-import {Label, TextInput} from "flowbite-react";
+import {Button, Label, TextInput} from "flowbite-react";
 
 export default function SignUp() {
     return (
         <div className='min-h-screen mt-20'>
             <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row
-            md:item-center">
+            md:item-center gap-5">
                 {/*left*/}
-                <div className=''>
+                <div className='flex-1'>
                     <Link to="/" className='font-bold dark:text-white text-4xl'>
                         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
                         via-purple-500 to-pink-500 rounded-lg text-white'>Blog</span>
@@ -19,11 +19,10 @@ export default function SignUp() {
                 </div>
                 
                 {/*right*/}
-                <div className="">
-                    <form>
+                <div className="flex-1">
+                    <form className='flex flex-col gap-4'>
                         <div>
-
-                            <Label htmlFor='username'/>
+                            <Label>Your username</Label>
                             <TextInput
                                 type='text'
                                 placeholder='Username'
@@ -31,7 +30,7 @@ export default function SignUp() {
                             />
                         </div>
                         <div>
-                            <Label value='Your email'/>
+                            <Label>Your email</Label>
                             <TextInput
                                 type='text'
                                 placeholder='Email'
@@ -39,14 +38,16 @@ export default function SignUp() {
                             />
                         </div>
                         <div>
-                            <Label value='Your password'/>
+                            <Label>Your password</Label>
                             <TextInput
                                 type='text'
                                 placeholder='Password'
                                 id='password'
                             />
                         </div>
-
+                        <Button gradientDuoTone="purpleToPink" type='submit'>
+                            Sign up
+                        </Button>
                     </form>
                 </div>
             </div>
