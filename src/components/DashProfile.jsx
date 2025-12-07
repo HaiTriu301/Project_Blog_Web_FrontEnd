b59b72d7b3062033fc9082ba2013e3e1f37366d7
HaiTriu301 <haitriuwork30@gmail.com>
2025-12-07 17:16:13 +0700

    add and create post page for admin

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -7,9 +7,10 @@ import {
     signoutSuccess
 } from "../redux/user/userSlice.js";
 import {HiOutlineExclamationCircle} from "react-icons/hi";
+import {Link} from "react-router-dom";
 
 export default function DashProfile() {
-    const {currentUser, error} = useSelector((state) => state.user);
+    const {currentUser, error, loading} = useSelector((state) => state.user);
 
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
@@ -219,9 +220,24 @@ export default function DashProfile() {
                     onChange={handleChange}
                 />
 
-                <Button type="submit" gradientduotone="purpleToBlue" outline>
-                    Update
+                <Button type='submit'
+                        gradientduotone="purpleToBlue"
+                        outline
+                        disabled={loading || imageFileUploading}
+                >
+                    {loading ? 'Updating...' : 'Update'}
                 </Button>
+                {currentUser.isAdmin && (
+                    <Link to={'/create-post'}>
+                        <Button
+                            type='button'
+                            gradientduotone='purpleToPink'
+                            className='w-full'
+                        >
+                            Create a post
+                        </Button>
+                    </Link>
+                )}
             </form>
 
             <div className="text-red-500 flex justify-between mt-5">
