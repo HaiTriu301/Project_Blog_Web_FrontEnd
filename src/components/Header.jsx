d9d849d6b1f1766ae5a895a67c6b579bdc328ab4
HaiTriu301 <haitriuwork30@gmail.com>
2025-12-08 23:15:31 +0700

    complete search and home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,18 +9,38 @@ import {
     NavbarToggle,
     TextInput
 } from "flowbite-react";
-import {Link, useLocation} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import {AiOutlineSearch} from "react-icons/ai";
 import {FaMoon, FaSun} from "react-icons/fa";
 import {useSelector, useDispatch} from "react-redux";
 import {toggleTheme} from "../redux/theme/themeSlice";
 import {signoutSuccess} from "../redux/user/userSlice.js";
+import {useEffect, useState} from "react";
 
 export default function Header() {
     const path = useLocation().pathname;
+    const location = useLocation();
     const dispatch = useDispatch();
     const {currentUser} = useSelector(state => state.user)
     const {theme} = useSelector(state => state.theme)
+    const [searchTerm, setSearchTerm] = useState('')
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromURL = urlParams.get('searchTerm');
+        if (searchTermFromURL) {
+            setSearchTerm(searchTermFromURL);
+        }
+    }, [location.search]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
 
     const handleSignout = async () => {
         try{
@@ -47,12 +67,14 @@ export default function Header() {
                 Fruit
             </Link>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <TextInput
                     type='text'
                     placeholder='Search...'
                     rightIcon={AiOutlineSearch}
                     className='hidden lg:inline'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
             </form>
 
